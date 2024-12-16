a6e43a284ccf66cb28f961bd544b6832dc52de76
idanvep <idanvapr123@gmail.com>
2024-12-16 12:13:01 +0200

    added a utils.cpp file and put in there all the utility functions from the main to make the code more accessible and organized

--- main.cpp
+++ main.cpp
@@ -625,57 +625,6 @@ void loadUsers(list<shared_ptr<User>>& userList)
 }
 // Files functions END -------------------------------------------------------------------------------------------------
 
-bool is9DigitInt(int num)
-{
-    if(num >= 100000000 && num <= 999999999)
-        return true;
-    return false;
-}
-/// Function to check if string input is using only letters
-/// \return acceptable value (string with letters)
-string getValidString()
-{
-    string value;
-    while (true)
-    {
-        getline(cin, value); // read the entire line
-        bool isValid = true;
-
-        for (char c : value)
-        {
-            if (!isalpha(c)) // check if the character is not a letter
-            {
-                isValid = false;
-                break;
-            }
-        }
-
-        if (isValid)
-            break; // exit loop if input is valid
-        else
-            cerr << "Invalid input. Please enter letters only." << endl;
-    }
-    return value;
-}
-/// Function to check if integer input is indeed a integer
-/// \return = acceptable value (integer)
-int getValidInt()
-{
-    int value;
-    while(true)
-    {
-        if(cin >> value) // successful read
-            break;
-        else
-        {
-            cerr << "Invalid input. please enter a valid input." << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignore invalid input
-        }
-    }
-    return value;
-}
-
 void deleteCandidate(list<shared_ptr<User>> &userList, shared_ptr<User> &currentUser, list<shared_ptr<Job_Submission>> &jobs_Submission_List)
 {
     string name = currentUser->getFirstName();
--- utils.h
+++ utils.h
@@ -1,11 +1,14 @@
+//
+// Created by idanv on 16/12/2024.
+//
+
 #ifndef UTILS_H
 #define UTILS_H
-
 #include <iostream>
 #include <limits>
 using namespace std;
-
-// Declare the function
+bool is9DigitInt(int num);
+string getValidString();
 int getValidInt();
 
-#endif // UTILS_H
+#endif //UTILS_H
